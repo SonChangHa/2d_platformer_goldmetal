3f1cf408d1446a1679319a8fc5deb322e4300127
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2023-05-21 14:28:56 +0900

    rr

--- Assets/Scripts/GroundMove.cs
+++ Assets/Scripts/GroundMove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GroundMove : MonoBehaviour
 {
-
+    Vector3 moveVec;
+    float runtime;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,8 @@ public class GroundMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        runtime += Time.deltaTime;
+        moveVec = new Vector3(0, Mathf.Sin(runtime), 0);
+        transform.position += moveVec;
     }
 }
