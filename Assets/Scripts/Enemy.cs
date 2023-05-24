be7a83bd4af507b7755e55455ed1e6cdd6161cd1
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2023-05-24 19:10:36 +0900

    오류 수정
    +를 *로 써버림

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int forward = 0;
+    int forward = -1;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +16,19 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        JumpCheck();
+        transform.Translate(new Vector2(forward * speed * Time.deltaTime, 0));
+        FindGround();
     }
 
-    void JumpCheck()
+    void FindGround()
     {
-        Debug.DrawRay(new Vector2(transform.position.x * forward, transform.position.y), -transform.up, Color.red);
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x * forward, transform.position.y), -transform.up, 1);
+        //0.5만큼만 앞에 레이캐스트가 생성되게
+        Debug.DrawRay(new Vector2(transform.position.x + forward, transform.position.y), -transform.up, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + forward, transform.position.y), -transform.up, 1);
 
-        if (hit.collider != null)
+        if (hit.collider == null)
         {
-
+            forward *= -1;
         }
 
     }
